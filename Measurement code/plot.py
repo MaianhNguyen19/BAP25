10aa8cc8094de494fd7db5a15bccc02796f52715
harimsuleman <120187547+harimsuleman@users.noreply.github.com>
2025-05-15 14:35:39 +0200

    code for the measurements_freq_sec

--- Measurement code/plot.py
+++ Measurement code/plot.py
@@ -1,15 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import math
 
-df = pd.read_csv('grip_data_1.csv')
-df = df.head(100)
+# Change as needed
+measurement_time_sec = 10
+signal_frequency = 150
+attempt_nr = 5
+
+#samples_for_1_sec = math.ceil(1/ 370e-6)
+#amount_of_samples = measurement_time_sec * samples_for_1_sec
+
+#path = r"C:\Users\harim\Desktop\grip_code\BAP25\Measurement code\Measurements"
+#foldername = "Measurements"
+filename = "humantenna"
+filename_1 = f"measurement_{signal_frequency}Hz_{measurement_time_sec}s_attempt{attempt_nr}.csv"
+#file_path = os.path.join(path, filename)
+
+
+df = pd.read_csv(filename)
+#df = df.head(100)
 
 # Plot grip vs time
 plt.figure(figsize=(10, 5))
-plt.plot(df['relative_time_us'], df['grip_voltage'], marker='o', linestyle='-')
-plt.xlabel('Time (microseconds)')
+plt.plot(df['relative_time_seconds'], df['grip_voltage'], marker='o', linestyle='-')
+plt.xlabel('Time (Seconds)')
 plt.ylabel('Grip Strength')
-plt.title('Grip Strength Over First X Samples')
+plt.title(f"measurement_{signal_frequency}Hz_{measurement_time_sec}s_attempt{attempt_nr}")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
